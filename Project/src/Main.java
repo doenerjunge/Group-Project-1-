d03d1a8c039fbd84fdef2cd62f4931e8b57ae314
YYQQSTG <YYQQSTG@CCSD-45MWBH2.ccsdut.net>
2019-02-01 09:01:23 -0700

    deleted test code

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -2,12 +2,6 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello");
-		System.out.println("yeet");
-		System.out.println("yeet");
-		for(int i = 0; i < 5; i++)
-		{
-			System.out.print("yes ");
-		}
+		
 	}
 }
