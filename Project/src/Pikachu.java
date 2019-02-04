8ff4402f1bae993275ffa69486552779e1b9e7fa
YYQQSTG <YYQQSTG@CCSD-45MWBH2.ccsdut.net>
2019-02-04 09:23:09 -0700

    improvements

--- Project/src/Pikachu.java
+++ Project/src/Pikachu.java
@@ -7,11 +7,16 @@ public class Pikachu extends Pokemon
 	{
 		super("Pikachu", "Electric", 40);
 		attacks = new HashMap<String, Integer>();
+		attacks.put("thunderbolt", 30);
 	}
 	
 	public void attack(Pokemon other)
 	{
-		System.out.println("Pikachu attacked " + other.getName() + " with thunderbolt.");
-		other.loseHealth(30);
+		String attck = readLine("Select attack");
+		if(attck.equals("thunderbolt"));
+		{
+			System.out.println("Pikachu attacked " + other.getName() + " with thunderbolt.");
+			other.loseHealth(attacks.get("thunderbolt"));
+		}
 	}
 }
