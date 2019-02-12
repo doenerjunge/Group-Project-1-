71a7ca89eebcbca1b4c307e128b58e15967c0d5a
YYQQSTG <YYQQSTG@CCSD-45MWBH2.ccsdut.net>
2019-02-12 09:32:33 -0700

    Finnished Attack HashMaps

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -2,6 +2,8 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-
+		Pokemon unusable = new Pokemon();
+		Pokemon p = new Pokemon("Pikachu");
+		System.out.println(p.getType());
 	}
 }
