f5b8a79388c03f8fd07bc0a1cad60454db40908d
JMHBYEU <JMHBYEU@CCSD-45CWBH2.ccsdut.net>
2019-02-13 09:27:05 -0700

    fixed main static method

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -9,6 +9,11 @@ public class Main
 
 	public static void main(String[] args)
 	{
+		new Main();
+	}
+	
+	public Main()
+	{
 		//initframe();
 		Pokemon unusable = new Pokemon();
 		Pokemon p = new Pokemon("Pikachu");
