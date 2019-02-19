aefaee4db24d7bc5a7100e7d49b4249311d9df0e
yyqqstg <yyqqstg@CCSD-45MWBH2.ccsdut.net>
2019-02-19 10:21:00 -0700

    that

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,9 +1,10 @@
-import java.awt.BorderLayout;
+import java.awt.Dimension;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
 
 public class Main
 {
@@ -11,10 +12,13 @@ public class Main
 	User me = new User();
 	
 	private JFrame Screen;
+	SpringLayout layout = new SpringLayout();
 	private Panel pnl;
-	private JLabel lbl;
 	private static JTextArea jText;
-	//ImageIcon icon = new ImageIcon("https://images.pexels.com/photos/2334/hill-meadow-tree-green.jpg?auto=compress&cs=tinysrgb&h=750&w=1260", "beautyIncarnate");
+	private JButton jbutOne;
+	private JButton jbutTwo;
+	private JButton jbutThree;
+	ImageIcon icon = new ImageIcon("https://images.pexels.com/photos/2334/hill-meadow-tree-green.jpg?auto=compress&cs=tinysrgb&h=750&w=1260", "beautyIncarnate");
 	
 	public static void main(String[] args)
 	{
@@ -23,7 +27,7 @@ public class Main
 	
 	public Main()
 	{
-		initframe();
+		initialize();
 		println("Hello young trainer, I am Professor Git, feel free to select your first Minimon");
 		
 	}
@@ -43,44 +47,40 @@ public class Main
 		jText.append(message + "\n");
 	}
 	
-	public void initframe()
+	public void initialize()
 	{
 		Screen = new JFrame();
 		Screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Screen.setLayout(new BorderLayout());
+		Screen.setLayout(layout);
 		Screen.setVisible(true);
 		Screen.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		Screen.pack();
-		Screen.repaint();
-		initpanel();
-		//initlabel();
-		initjText();
-	}
-	 
-	public void initjText() 
-	{
+		
+		pnl = new Panel();
+		pnl.setLayout(new SpringLayout());
+		pnl.setPreferredSize(new Dimension(500, 30));
+		layout.putConstraint(SpringLayout.NORTH, pnl, 0, SpringLayout.NORTH, Screen);
+		Screen.add(pnl, SpringLayout.NORTH);
+		
+		jbutOne = new JButton("One");
+		jbutTwo = new JButton("Two");
+		jbutThree = new JButton("Three");
+		jbutOne.setPreferredSize(new Dimension(100, 100));
+		jbutTwo.setPreferredSize(new Dimension(100, 100));
+		jbutThree.setPreferredSize(new Dimension(100, 100));
+		layout.putConstraint(SpringLayout.SOUTH, jbutOne, 700, SpringLayout.SOUTH, Screen);
+		layout.putConstraint(SpringLayout.SOUTH, jbutTwo, 800, SpringLayout.SOUTH, Screen);
+		layout.putConstraint(SpringLayout.SOUTH, jbutThree, 900, SpringLayout.SOUTH, Screen);
+		layout.putConstraint(SpringLayout.NORTH, pnl, 0, SpringLayout.NORTH, Screen);
+		Screen.add(jbutOne, SpringLayout.SOUTH);
+		Screen.add(jbutTwo, SpringLayout.SOUTH);
+		Screen.add(jbutThree, SpringLayout.SOUTH);
+		
 		jText = new JTextArea();
 		jText.append( "Welcome to the world of Minimon.\n" );
 		Screen.add(jText);
-	}
-	
-	/*
-	private void initlabel() 
-	{
-		lbl = new JLabel(icon);
-		Screen.add(lbl);
+		
+		
 		Screen.pack();
 		Screen.repaint();
 	}
-	*/
-
-	public void initpanel()
-	{
-		pnl = new Panel();
-		pnl.setLayout(new BorderLayout());
-		Screen.add(pnl);
-		Screen.pack();
-		Screen.repaint();
-	}
-	
 }
